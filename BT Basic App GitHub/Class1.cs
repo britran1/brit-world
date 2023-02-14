96cc74982cb499edfe6ebd782feb324cf63ca414
britt <britt@SAMSUNGCOMPUTER>
2023-02-14 01:08:09 -0600

    Add project files.

---
+++ BT Basic App GitHub/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BT_Basic_App_GitHub
+{
+    public class Class1
+    {
+        public String Name;
+        public Class1()
+        {
+            Name = "[Brittney Tran] - The Code Master";
+        }
+    }
+}
---
+++ BasicAppTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicAppTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicAppTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d1bff48-fe57-4769-bbdd-11ceb6aa3f22")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
